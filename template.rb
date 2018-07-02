4563358e98e0744c8d85eee149690d6e8a90a358
Paul <pauloptimised@users.noreply.github.com>
2018-07-02 14:46:37 +0100

    Simplify

--- template.rb
+++ template.rb
@@ -2,7 +2,7 @@ gem_group :development do
   gem 'optimadmin_generators', git: 'git@github.com:eskimosoup/optimadmin_generators.git'
 end
 
-gem 'optimadmin', git: 'git@github.com:eskimosoup/Optimadmin.git', branch: 'master'
+gem 'optimadmin', git: 'git@github.com:eskimosoup/Optimadmin.git', branch: :master
 
 application(nil, env: 'production') do
   <<-RUBY
@@ -21,11 +21,8 @@ end
 run 'bundle install'
 
 after_bundle do
-  rake 'optimadmin:install:migrations'
   # Generator hangs if you don't stop spring
   run 'spring stop'
-  generate 'optimadmin:site_settings'
-  generate 'optimadmin:install'
-
-  append_to_file 'config/initializers/assets.rb', 'Rails.application.config.assets.precompile += %w( optimadmin/* )'
+  generate 'optimadmin:setup'
+  rails_command('db:migrate')
 end
